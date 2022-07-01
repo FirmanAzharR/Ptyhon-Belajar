5b5e6c746a45b61abe79ae67fb8a32d8378d5aa3
FirmanAzharR <firmanazhar14@gmail.com>
2022-07-01 11:04:39 +0700

    first commit

---
+++ belajar-python.py
@@ -0,0 +1,37 @@
+#print
+print("belajar pyton")
+#tipe data dictionary
+saya = {"nama":"Firman", "umur": 17}
+print(saya)
+#check type
+print(type(saya))
+
+#ifelse
+bilangan=10
+
+if(bilangan>10):
+    print('halo')
+elif(bilangan<10):
+    print('hai')
+else:
+    print('yoyoi')
+
+#loop
+list = [1,2,3,4,5,6,7,8,9]
+for x in list:
+    print(x)
+
+#list
+list = ['fisika','kimia',10,20]
+print("value list ke 3 :" ,list[2])
+print ("list[0:2]: ", list[0:2])
+#update list 
+list[1] = 'matematika'
+print(list)
+#delete list 
+del list[3]
+print(list)
+#addnew list
+list.append = 30
+print(list)
+
